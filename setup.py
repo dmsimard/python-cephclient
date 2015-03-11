1f75b173b85cc107e7bdb3be4629e7916a9851d9
David Moreau Simard <dmsimard@iweb.com>
2015-03-11 11:03:16 -0400

    Tag python-cephclient as beta instead of alpha
    It's pretty much beyond alpha now.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['lxml>=3.2.5', 'requests>=2.2.1'],
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Intended Audience :: Information Technology',
